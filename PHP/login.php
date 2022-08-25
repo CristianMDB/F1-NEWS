cbe16a663cc3959c2f55bffaef55b912d170a63a
Cristian Mendez Del Boca <62318871+CristianMDB@users.noreply.github.com>
2022-08-25 20:35:14 +0200

    Update login.php

--- PHP/login.php
+++ PHP/login.php
@@ -1,12 +1,9 @@
 <?php
 
 function comprobar_usuario($usuario, $clave){
-    //$res = leer_config(dirname(__FILE__) . "\configuracion.xml", dirname(__FILE__) . "\configuracion.xsd");
     $db = new PDO("mysql:@eu-cdbr-west-03.cleardb.net/heroku_8a34a067c8d1005". "bf711ef804d762", "6f2f459b");
     $ins = "select CodUsu,Usuario from Usuarios where  Usuario = " . "'$usuario'" . " and Contraseña = " . "'$clave'";
     $resul = $db->query($ins);
-
-
     if ($resul->rowCount() === 1) {
         return $resul->fetch();
     } else {

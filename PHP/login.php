5e4035505595de260f2fac8e8975477142d3f837
Cristian Mendez Del Boca <62318871+CristianMDB@users.noreply.github.com>
2022-08-25 20:34:26 +0200

    Update login.php

--- PHP/login.php
+++ PHP/login.php
@@ -1,34 +1,8 @@
 <?php
 
-//CONEXION CON LA BASE DE DATOS
-function leer_config($ficheroXML, $esquema){
-
-    $config = new DOMDocument();
-    $config->load($ficheroXML);
-    $res = $config->schemaValidate($esquema);
-
-    if ($res == FALSE) {
-        throw new InvalidArgumentException("Revisa ficheros de configuración.");
-    }
-    
-    $datos = simplexml_load_file($ficheroXML);
-    $ip = $datos->xpath("//ip");
-    $nombre = $datos->xpath("//nombre");
-    $usuario = $datos->xpath("//usuario");
-    $clave = $datos->xpath("//clave");
-    $cad = sprintf("mysql:dbname=%s;host=%s", $nombre[0], $ip[0]);
-    $resul = [];
-    $resul[] = $cad;
-    $resul[] = $usuario[0];
-    $resul[] = $clave[0];
-
-    return $resul;
-}
-
-//COMPRUEBA QUE EL USUARIO Y LA CONTRASEÑA DEL FORMULARIO LOGIN COINCIDE CON ALGUNO EXISTENTE EN LA BASE DE DATOS
 function comprobar_usuario($usuario, $clave){
-    $res = leer_config(dirname(__FILE__) . "\configuracion.xml", dirname(__FILE__) . "\configuracion.xsd");
-    $db = new PDO($res[0], $res[1], $res[2]);
+    //$res = leer_config(dirname(__FILE__) . "\configuracion.xml", dirname(__FILE__) . "\configuracion.xsd");
+    $db = new PDO("mysql:@eu-cdbr-west-03.cleardb.net/heroku_8a34a067c8d1005". "bf711ef804d762", "6f2f459b");
     $ins = "select CodUsu,Usuario from Usuarios where  Usuario = " . "'$usuario'" . " and Contraseña = " . "'$clave'";
     $resul = $db->query($ins);
 
